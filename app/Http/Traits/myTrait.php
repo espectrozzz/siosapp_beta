c5ae3e4a9b1b04bd4ad87b1778f8977f7ce41578
espectrozzz <34382426+espectrozzz@users.noreply.github.com>
2021-12-02 13:14:59 -0600

    Cambio de Orden OT en script

--- app/Http/Traits/myTrait.php
+++ app/Http/Traits/myTrait.php
@@ -45,11 +45,12 @@ $bddUbicacion->ubicacion = $direccion;
 $bddUbicacion->save();
 
     //Cuerpo de mensaje
-            $mensaje = '*FOLIO*:' . $analisis->folio . "\n";
-        if($analisis->OT){
-            $mensaje .= '*OT*: '. $analisis->OT . "\n";      // Verifica si el folio tiene capturado OT
-        }
-            $mensaje .= $consulta[0]->tfolio . "\n".
+            $mensaje = '*FOLIO*:' . $analisis->folio . "\n".
+                        $consulta[0]->tfolio . "\n";
+            if($analisis->OT){
+                $mensaje .= '*OT*: '. $analisis->OT . "\n";      // Verifica si el folio tiene capturado OT
+            }
+            $mensaje.=
             '*Distrito*: '. $consulta[0]->distrito. "\n".
             '*Cluster*: '. $consulta[0]->cluster . "\n".
             '*OLT*: '. $analisis->olt . "\n".
