6824d4ccd1c372c70ca8f8e4608e20315397ad85
espectrozzz <34382426+espectrozzz@users.noreply.github.com>
2021-12-02 15:58:14 -0600

    Corregir OT

--- app/Models/c_incidencia.php
+++ app/Models/c_incidencia.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class c_incidencia extends Model
 {
     use HasFactory;
+
+    public function tFolios(){
+        return $this->belongsToMany(c_tipo_folio::class, 'incidencia_has_tfolios', 'incidencia_id', 'tfolio_id');
+    }
 }
